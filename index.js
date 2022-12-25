e778c225c4f8396d2d210ed9aedd05d10fd55371
Tanmay Rane <95012133+tanmayrane18@users.noreply.github.com>
2022-12-25 20:03:50 +0530

    main files are added

---
+++ index.js
@@ -0,0 +1,29 @@
+$(".shift").on("click",function(){
+    $(".shift").toggleClass("changePosition");
+    $(".fixedButtonContent").toggleClass("showButton");
+    $(".fixedButtonsymbol").toggleClass("rotateSymbol");
+});
+
+
+// var x = window.matchMedia("(max-width: 990px)")
+// deleteDrop(x) // Call listener function at run time
+// x.addListener(deleteDrop) // Attach listener function on state changes
+
+// function deleteDrop(x) {
+//     if (x.matches) { // If media query matches
+//       $("div").removeClass("hoverareamens");
+//       $("div").removeClass("hoverareawomens");
+//       $("div").removeClass("hoverareakids");
+//       $("div").removeClass("hoverareahome");
+//       $("div").removeClass("hoverareabeauty");
+//       $("div").removeClass("hoverareastudio");
+//       $("div").removeClass("hoverareaprofile");
+//     } else {
+//       document.body.style.backgroundColor = "blue";
+//     }
+//   }
+
+  
+
+
+
